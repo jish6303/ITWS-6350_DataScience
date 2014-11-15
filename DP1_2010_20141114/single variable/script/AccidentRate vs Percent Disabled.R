174960dd9014e4b9caa535591566336820fceda7
jish6303 <jiaju.shen@colorado.edu>
2014-11-15 11:10:35 -0500

    Created a new directory

--- DP1_2010_20141114/single variable/script/AccidentRate vs Percent Disabled.R
+++
@@ -1,9 +0,0 @@
-Attributes<-read.table("Census 2010 DP1 DP2 and DP3 - Extracted Attributes.csv", header=T, sep=',')
-fatality<-read.table("FatalititesDatasetByYear.csv", header=T, sep=',')
-F10 <- fatality[, c(1,12)]
-data10<-merge(F10, Attributes, by="State")
-rate<-data10$Y2010/data10$Total.population
-plot(data10$Percent.Disabled, xlab= "Percent never married",rate, ylab= "Accident Rate", main="Accident Rate vs.Percentage Disabled")
-model<-lm(rate~data10$Percent.Disabled)
-abline(model, col="blue")
-summary(model)
--- DP1_2010_20141114/single variable/script/AccidentRate vs State Population.R
+++
@@ -1,9 +0,0 @@
-Attributes<-read.table("Census 2010 DP1 DP2 and DP3 - Extracted Attributes.csv", header=T, sep=',')
-fatality<-read.table("FatalititesDatasetByYear.csv", header=T, sep=',')
-F10 <- fatality[, c(1,12)]
-data10<-merge(F10, Attributes, by="State")
-rate<-data10$Y2010/data10$Total.population
-plot(data10$Total.population,rate, xlab= "State Population", ylab= "Accident Rate", main="Accident Rate vs.State Total Population")
-model<-lm(rate~data10$Total.population)
-abline(model, col="blue")
-summary(model)
